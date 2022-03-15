b040d68df1e3e1c02687ef73d163f1a23d7d00b0
Le Van Thuan My <51257500+levanthuanmy@users.noreply.github.com>
2022-03-15 15:06:47 +0700

    set up firebase_core && firebase_database for android

--- lib/main.dart
+++ lib/main.dart
@@ -6,30 +6,47 @@ import 'package:flutter_chat_app/screens/signup_screen.dart';
 
 import 'constants/ui_constant.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: UIConstant.createMaterialColor(UIConstant.primary),
-      ),
-      debugShowCheckedModeBanner: false,
-      routes: {
-        "/signin": (context) => const SignInScreen(),
-        "/signup": (context) => const SignUpScreen(),
-        "/home": (context) => const HomeScreen(),
-        "/friends": (context) => const FriendListScreen()
-      },
-      home: const HomeScreen(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: UIConstant.createMaterialColor(UIConstant.primary),
+        ),
+        debugShowCheckedModeBanner: false,
+        routes: {
+          "/signin": (context) => const SignInScreen(),
+          "/signup": (context) => const SignUpScreen(),
+          "/home": (context) => const HomeScreen(),
+          "/friends": (context) => const FriendListScreen()
+        },
+        // home: const HomeScreen(),
+        home: FutureBuilder(
+          future: _fbApp,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasError) {
+              print('[ERROR] ${snapshot.error.toString()}');
+              return Text('Something went wrong!');
+            } else if (snapshot.hasData) {
+              return MyHomePage();
+            } else
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+          },
+        ));
   }
 }
 
@@ -43,6 +60,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text('hello')],
+      ),
+    ));
   }
 }
